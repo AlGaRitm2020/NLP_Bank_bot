16b556af3eb4fa3eb99b438418956783917ce64d
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-06-03 20:36:12 +0400

    added get balance function

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from random import randint
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, \
+    ConversationHandler
 
 TOKEN = '1779872877:AAGKs54Jotb37C0E7TYe0qx1KMMMkqLEYwk'
 
+
 def start(update: Update, context: CallbackContext):
-
-
     update.message.reply_text('Добро пожаловать! \n' +
-                              'Я телеграм бот-помощник NLP-bank. Я могу показать инормацию  о банке, посмотреть бадланс, перевести деньги, забокировать карту, написать в поддержку')
+                              'Я телеграм бот-помощник NLP-bank. Я могу показать инормацию \
+                                о банке, посмотреть бадланс, перевести деньги, забокировать \
+                                карту, написать в поддержку')
 
 
 def info(update: Update, context: CallbackContext):
     update.message.reply_text('ООО NLP Bank')
 
+
 def enter_card_number(update: Update, context: CallbackContext):
     update.message.reply_text("Введите номер своей карты:")
     print('enter 1')
     return 1
+
 
 def enter_pin_code(update: Update, context: CallbackContext):
     print('enter 2')
@@ -26,17 +30,20 @@ def enter_pin_code(update: Update, context: CallbackContext):
     update.message.reply_text("Введите пин код карты:")
     return 2
 
+
 def block(update: Update, context: CallbackContext):
     print('enter 3')
     global card_num
     pin_code = update.message.text
     update.message.reply_text("Карта " + card_num + ' заблокирована')
-    return 2
+    return ConversationHandler.END
+
 
 def start_transfer(update: Update, context: CallbackContext):
     update.message.reply_text("Введите номер вашей карты")
     print('enter 1')
     return 1
+
 
 def enter_cvv_code(update: Update, context: CallbackContext):
     global card_num
@@ -44,38 +51,51 @@ def enter_cvv_code(update: Update, context: CallbackContext):
     update.message.reply_text("Введите cvv код карты:")
     return 2
 
+
 def enter_addressee_name(update: Update, context: CallbackContext):
     global cvv_code
     cvv_code = update.message.text
     update.message.reply_text("Введите номер карты адресата")
     return 3
+
+
 def enter_amount(update: Update, context: CallbackContext):
     global addressee_card
     addressee_card = update.message.text
     update.message.reply_text("Введите сумму перевода")
     return 4
+
+
 def send_money(update: Update, context: CallbackContext):
     global addressee_card, cvv_code, card_num
     ammount = update.message.text
-    update.message.reply_text(f"Совершен перевод с карты {card_num} на карту {addressee_card} в размере {ammount} рублей")
-    return 5
+    update.message.reply_text(
+        f"Совершен перевод с карты {card_num} на карту {addressee_card} в размере {ammount} рублей")
+    return ConversationHandler.END
+
 
 def get_balance(update: Update, context: CallbackContext):
     balance = randint(0, 100000)
-    update.message.reply_text("Ва")
+    update.message.reply_text(f"Ваш баланс: {balance} рублей")
+    return ConversationHandler.END
 
-def read(update, context):
 
-
+def stream(update, context):
     if update.message.text == 'block':
         reply_keyboard = [['/block']]
         markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-        update.message.reply_text('Если вы хотите заблокировать карту нажмите /block', reply_markup=markup)
+        update.message.reply_text('Если вы хотите заблокировать карту нажмите /block',
+                                  reply_markup=markup)
     elif update.message.text == 'transfer':
         reply_keyboard = [['/transfer']]
         markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-        update.message.reply_text('Если вы хотите совершить перевод карту нажмите /transfer', reply_markup=markup)
-
+        update.message.reply_text('Если вы хотите совершить перевод карту нажмите /transfer',
+                                  reply_markup=markup)
+    elif update.message.text == 'balance':
+        reply_keyboard = [['/balance']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        update.message.reply_text('Чтобы узнать баланс вашей карты нажмите /balance',
+                                  reply_markup=markup)
 
 
 # def block(update: Update, context: CallbackContext):
@@ -87,7 +107,7 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler("info", info))
     dispatcher.add_handler(CommandHandler("balance", get_balance))
 
-    Dialog_block = ConversationHandler(
+    dialog_block = ConversationHandler(
         entry_points=[CommandHandler('block', enter_card_number)],
         states={
             1: [MessageHandler(Filters.text, enter_pin_code)],
@@ -96,8 +116,8 @@ def main() -> None:
         fallbacks=[MessageHandler(Filters.text, start)]
     )
 
-    Dialog_transfer = ConversationHandler(
-        entry_points=[CommandHandler('start_transfer', enter_card_number)],
+    dialog_transfer = ConversationHandler(
+        entry_points=[CommandHandler('transfer', enter_card_number)],
         states={
             1: [MessageHandler(Filters.text, enter_cvv_code)],
             2: [MessageHandler(Filters.text, enter_addressee_name)],
@@ -107,32 +127,21 @@ def main() -> None:
         fallbacks=[MessageHandler(Filters.text, start)]
     )
 
-    Dialog_balance = ConversationHandler(
+    dialog_balance = ConversationHandler(
         entry_points=[CommandHandler('balance', enter_card_number)],
         states={
             1: [MessageHandler(Filters.text, enter_pin_code)],
-            2: [MessageHandler(Filters.text, block)],
+            2: [MessageHandler(Filters.text, get_balance)],
         },
         fallbacks=[MessageHandler(Filters.text, start)]
     )
-    #
-    # Dialog_theory = ConversationHandler(
-    #     entry_points=[CommandHandler('theory', conv_begin)],
-    #     states={
-    #         1: [MessageHandler(Filters.text, theory)],
-    #     },
-    #     fallbacks=[MessageHandler(Filters.text, start)]
-    # )
-    # # dispatcher.add_handler(CommandHandler("photo", send_photo))
-    dispatcher.add_handler(Dialog_block)
-    dispatcher.add_handler(Dialog_transfer)
-    # dispatcher.add_handler(Dialog_theory)
 
-    # dispatcher.add_handler(MessageHandler(Filters.text, help_command))
+    dispatcher.add_handler(dialog_block)
+    dispatcher.add_handler(dialog_transfer)
+    dispatcher.add_handler(dialog_balance)
 
 
-    # dispatcher.add_handler(MessageHandler(Filters.text, echo))
-    text_handler = MessageHandler(Filters.text, read)
+    text_handler = MessageHandler(Filters.text, stream)
 
     # Регистрируем обработчик в диспетчере.
     dispatcher.add_handler(text_handler)
