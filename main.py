371681d7344831c5361a9a313e2bddcd1402954c
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-06-04 13:21:28 +0400

    added help function

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ TOKEN = '1779872877:AAFM0z3EPu23T169XtMcD7DUEvfcRYSb2H4'
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text('Добро пожаловать! \n' +
-                              'Я телеграм бот-помощник NLP-bank. Я могу показать инормацию '+
-                                'о банке, посмотреть бадланс, перевести деньги, забокировать' +
-                                'карту, написать в поддержку. \n' + 'Для начала работы введите'+
-                                ' номер вашей карты')
+                              'Я телеграм бот-помощник NLP-bank. Я могу показать инормацию ' +
+                              'о банке, посмотреть бадланс, перевести деньги, забокировать' +
+                              'карту, написать в поддержку. \n' + 'Для начала работы введите' +
+                              ' номер вашей карты')
 
     return 1
 
 
 def info(update: Update, context: CallbackContext):
-    update.message.reply_text('ООО NLP Bank')
+    update.message.reply_text('NLP-bank — крупнейший банк в России, Центральной и Восточной '
+                              'Европе, один из ведущих международных финансовых институтов')
 
 
 def enter_card_number(update: Update, context: CallbackContext):
-
     update.message.reply_text("Введите номер своей карты:")
     return 1
 
@@ -36,11 +36,13 @@ def enter_pin_code(update: Update, context: CallbackContext):
     update.message.reply_text("Введите пин код карты:")
     return 2
 
+
 def enter_cvv_code(update: Update, context: CallbackContext):
     global pin_code
     pin_code = update.message.text
     update.message.reply_text("Введите cvv код карты:")
     return 3
+
 
 def finish_login(update: Update, context: CallbackContext):
     global card_num
@@ -48,11 +50,13 @@ def finish_login(update: Update, context: CallbackContext):
     update.message.reply_text(f"Вы вошли в систему с картой {card_num}")
     return ConversationHandler.END
 
+
 def start_blocking(update: Update, context: CallbackContext):
     global card_num
     update.message.reply_text(f"Вы уверены что хотите заблокировать карту {card_num}?")
     update.message.reply_text("Введите пин код карты для подтверждения")
     return 1
+
 
 def finish_blocking(update: Update, context: CallbackContext):
     pin_code = update.message.text
@@ -60,6 +64,7 @@ def finish_blocking(update: Update, context: CallbackContext):
     markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     update.message.reply_text(f"Вы заблокировали карту {card_num}", reply_markup=markup)
     return ConversationHandler.END
+
 
 def block(update: Update, context: CallbackContext):
     global card_num
@@ -96,10 +101,26 @@ def get_balance(update: Update, context: CallbackContext):
     update.message.reply_text(f"Баланс карты {card_num} составляет {balance} рублей")
     return
 
+def start_help(update: Update, context: CallbackContext):
+    update.message.reply_text(f"Опишите вашу проблему как можно более подробно. Оператор увидит ваше сообщение")
+    return 1
+
+def send_feedback(update: Update, context: CallbackContext):
+    feedback = update.message.text
+    user = str(update.message.from_user.first_name) + str(update.message.from_user.last_name)
+    print(f'from: {user} \nmessage: {feedback}\n')
+    update.message.reply_text(f"Ваше сообщение отправлено")
+    return ConversationHandler.END
+
+
 
 def stream(update, context):
     stems = get_stems(update.message.text)
-
+    if check_stems(stems, KeyWords.help):
+        reply_keyboard = [['/help']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        update.message.reply_text('Если у вас возникла проблема нажмите /help',
+                                  reply_markup=markup)
     if check_stems(stems, KeyWords.block):
         reply_keyboard = [['/block']]
         markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
@@ -116,8 +137,6 @@ def stream(update, context):
         update.message.reply_text('Чтобы узнать баланс вашей карты нажмите /balance',
                                   reply_markup=markup)
 
-
-# def block(update: Update, context: CallbackContext):
 
 def main() -> None:
     updater = Updater(TOKEN)
@@ -153,14 +172,19 @@ def main() -> None:
         },
         fallbacks=[MessageHandler(Filters.text, start)]
     )
+    dialog_help = ConversationHandler(
+        entry_points=[CommandHandler('help', start_help)],
+        states={
+            1: [MessageHandler(Filters.text, send_feedback)],
 
-
+        },
+        fallbacks=[MessageHandler(Filters.text, start)]
+    )
 
     dispatcher.add_handler(dialog_start)
     dispatcher.add_handler(dialog_block)
     dispatcher.add_handler(dialog_transfer)
-
-
+    dispatcher.add_handler(dialog_help)
 
     text_handler = MessageHandler(Filters.text, stream)
 
